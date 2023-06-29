8c7302301d1b58a804d49c38aae9acdb82ac2ff1
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-06-29 11:52:38 -0400

    added unique id function

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+// const db = require('./db/db.json')
 
 const app = express();
 
@@ -9,11 +10,19 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /notes or /*'));
 
-app.get('/api/notes', (req, res) => 
+app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('/api/*', (req, res) => 
+app.get('/*', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html')));    
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data => res.json(JSON.parse(data))))
+});
+
+    
+// app.get('/api', (req, res) => res.json('db'));
 
 app.listen(PORT, () =>
     console.log(`Listening at http://localhost:${PORT}`))
