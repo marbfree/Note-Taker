c64ba74dea2e34ab488a5391b8608f15b5ad8f16
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-04 09:29:36 -0400

    added post and get api

--- server.js
+++ server.js
@@ -1,24 +1,52 @@
+// Imports libraries and files
 const express = require('express');
 const path = require('path');
-// const db = require('./db/db.json')
+const db = require('./db/db.json');
+// const {readFile, writeFile} = require('fs/promises');
 
+// Initializes express module
 const app = express();
 
+// uses port 3001
 const PORT = 3001
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+// Get method to respond with notes or index file
 app.get('/', (req, res) => res.send('Navigate to /notes or /*'));
 
+// Get method to send notes file
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+// Get method to send index file
 app.get('/*', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/index.html')));    
 
+// Reads file from db folder
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data => res.json(JSON.parse(data))))
+    console.info(`${req.method} request received for tips`);
+readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text, 
+            note_it: uuid(),
+        };
+    readAndAppend(newNote, './db/db.json');
+    res.json('Note added successfully');
+    } else{
+        res.error('Error in adding note');
+    }
 });
 
     
